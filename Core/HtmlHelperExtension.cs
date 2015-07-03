b1889e3a329f5642069af9aacbbe6b2b2a874d64
diogolmenezes@gmail.com <diogolmenezes@gmail.com>
2015-07-03 12:11:24 -0300

    Ajustes

--- Core/HtmlHelperExtension.cs
+++ Core/HtmlHelperExtension.cs
@@ -7,9 +7,12 @@ namespace MvcSmartFlash.Core
     {        
         public static IHtmlString RenderFlash(this HtmlHelper html)
         {
-            var cssClass = html.ViewContext.TempData["FlashMessage_CssClass"];
-            var message  = html.ViewContext.TempData["FlashMessage_Message"];
-            var flash    = string.Format("<div class=\"flash-message {0}\">{1}</div>", cssClass, message);
+            var cssClass = html.ViewContext.TempData["FlashMessage_CssClass"].ToString();
+            var message  = html.ViewContext.TempData["FlashMessage_Message"].ToString();
+            var flash    = "";
+
+            if(cssClass.HasValue() && message.HasValue())
+                flash = string.Format("<div class=\"flash-message {0}\">{1}</div>", cssClass, message);
 
             return MvcHtmlString.Create(flash);
         }
---
+++ Core/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MvcSmartFlash.Core
+{
+    public static class StringExtensions
+    {
+        public static bool HasValue(this string value)
+        {
+            return !String.IsNullOrEmpty(value);
+        }
+
+        public static bool DoesNotHasValue(this string value)
+        {
+            return !HasValue(value);
+        }
+    }
+}
